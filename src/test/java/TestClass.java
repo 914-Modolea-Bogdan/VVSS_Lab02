48f75f52b91c3bf58f4799d6084e90522ce9fc75
BogdanModolea <bogdan.modolea@stud.ubbcluj.ro>
2024-03-27 22:06:22 +0200

    Lab02_Code

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,32 @@
+import org.example.domain.Nota;
+import org.example.domain.Student;
+import org.example.domain.Tema;
+import org.example.repository.*;
+import org.example.service.Service;
+import org.example.validation.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+public class TestClass {
+
+    public static Service service;
+
+    @BeforeAll
+    public static void setup() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studentitest.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    }
+
+    @Test
+    public void testAddStudent() {
+        assert(service.saveStudent("5", "John", 932) == 0);
+    }
+}
